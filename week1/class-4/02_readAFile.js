d472f859b695ddd57e04fe383442e5f81c113687
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-08-12 15:06:32 +0530

    fs async readFile function

---
+++ week1/class-4/02_readAFile.js
@@ -0,0 +1,12 @@
+// We will read a file using the fs (filesystem) module in Node.js
+const fs = require("fs"); // This line imports the fs module which provides file system related functionalities
+
+// Note - Here we are passing a function without name, it is called anonymous function.
+
+// fs.readFile() is an async function!
+
+fs.readFile("a.txt", "utf-8", function (err, data) {
+  console.log(data); // This will print the content of a.txt file
+});
+
+console.log("Hello world!");   // This run first as it is not waiting for the file read to complete
