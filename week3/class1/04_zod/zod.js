3fdeb36edb8714cc713ebe4b65006f5f06632fbd
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-08-15 17:22:05 +0530

    basic zod validation

---
+++ week3/class1/04_zod/zod.js
@@ -0,0 +1,11 @@
+const zod = require('zod');
+
+function validateInput(arr){
+  const schema = zod.array(zod.number());
+
+  const response = schema.safeParse(arr);
+  console.log(response);
+}
+
+validateInput([1, 2, 3]);
+// validateInput([1, "2", 3]); Throws an error
