b7584dcae5cc93b8827309c7f064ba2f49d5fc2a
Ayush <ayushkumar85385@gmail.com>
2025-08-25 13:58:48 +0530

    request counter assignment done

--- assignments-cohort/week-3/01-middlewares/01-requestcount.js
+++ assignments-cohort/week-3/01-middlewares/01-requestcount.js
@@ -9,6 +9,11 @@ let requestCount = 0;
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
+app.use(function(req, res, next) {
+  requestCount++;
+  console.log(requestCount);
+  next();
+})
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -21,5 +26,9 @@ app.post('/user', function(req, res) {
 app.get('/requestCount', function(req, res) {
   res.status(200).json({ requestCount });
 });
+
+app.listen(3000, function() {
+  console.log(`Server is listening to https://localhost:3000/`);
+})
 
 module.exports = app;
