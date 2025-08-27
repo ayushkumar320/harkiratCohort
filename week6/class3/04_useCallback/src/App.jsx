cf4921708b7e53249ad76ca5b95978a15b9c344e
Ayush <ayushkumar85385@gmail.com>
2025-08-27 23:39:21 +0530

    use callback revision

---
+++ week6/class3/04_useCallback/src/App.jsx
@@ -0,0 +1,55 @@
+import {useEffect, useState, memo, useCallback} from "react";
+
+function App() {
+  const [exchange1Data, setExchange1Data] = useState({});
+  const [exchange2Data, setExchange2Data] = useState({});
+  const [bankData, setBankData] = useState({});
+
+  useEffect(() => {
+    // Some operation to get the data
+    setExchange1Data({
+      returns: 100,
+    });
+  }, []);
+
+  useEffect(() => {
+    // Some operation to get the data
+
+    setExchange2Data({
+      returns: 100,
+    });
+  }, []);
+
+  useEffect(() => {
+    // Some operation to get the data
+    setTimeout(() => {
+      setBankData({
+        income: 100,
+      });
+    }, 5000);
+  }, []);
+
+  // useCallback is about not rendering a child component, if the function hasnt/doesnt need to change across renders
+  // Basically useCallback is useMemo but for functions
+
+  const calculateCryptoReturn = useCallback(
+    function () {
+      return exchange1Data.returns + exchange2Data.returns;
+    },
+    [exchange1Data, exchange2Data]
+  );
+
+  return (
+    <div>
+      <CryptoGainsCalculator calculateCryptoReturn={calculateCryptoReturn} />
+    </div>
+  );
+}
+
+
+const CryptoGainsCalculator = memo(function Child({calculateCryptoReturn}) {
+  console.log("crypto child rendered");
+  return <div>Your crypto returns are {calculateCryptoReturn()}</div>;
+});
+
+export default App;
